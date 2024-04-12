e6e9a6c5729e08d5950ea3acc3e8b3656c4a5c14
seagull <150605383@qq.com>
2024-04-12 21:27:28 +0800

    引爆按钮

--- Assets/Roulette/Scripts/ViewCtrls/ActionsCtrl.cs
+++ Assets/Roulette/Scripts/ViewCtrls/ActionsCtrl.cs
@@ -1,31 +1,53 @@
+using System;
+using NUnit.Framework;
 using Roulette.Scripts.Models;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Roulette.Scripts.ViewCtrls
 {
     public class ActionsCtrl : MonoBehaviour
     {
+        [SerializeField] private GameObject content;
+        [SerializeField] private GameObject contentAttack;
+        [SerializeField] private GameObject contentDenonate;
+        [SerializeField] private Button attackButton;
+        [SerializeField] private Button denonateButton;
+
+        private void Start()
+        {
+            attackButton.onClick.AddListener(OnAttackClicked);
+            denonateButton.onClick.AddListener(OnDenonateClicked);
+        }
+
         public void EnableActionInput()
         {
+            content.SetActive(true);
+            contentAttack.SetActive(true);
+            contentDenonate.SetActive(true);
             // TODO: 提示玩家输入
             Debug.Log("按下S选择炸自己；按下O选择炸对方。");
         }
 
         public void DisableActionInput()
         {
+            content.SetActive(false);
+            contentAttack.SetActive(false);
+            contentDenonate.SetActive(false);
             // TODO: 禁止玩家输入
         }
 
-        private void Update()
+        void OnAttackClicked()
         {
-            // 功能完成后删除下面的草稿
-            if (Input.GetKey(KeyCode.S))
-                onPlayerAction.Invoke(new PlayerFiresGun(PlayerIndex.P1));
-            if (Input.GetKey(KeyCode.O))
-                onPlayerAction.Invoke(new PlayerFiresGun(PlayerIndex.P2));
+            onPlayerAction.Invoke(new PlayerFiresGun(PlayerIndex.P2));
         }
 
+        void OnDenonateClicked()
+        {
+            onPlayerAction.Invoke(new PlayerFiresGun(PlayerIndex.P1));
+        }
+        
         // TODO: 在玩家输入后发出事件
         public UnityEvent<PlayerFiresGun> onPlayerAction = new();
     }
